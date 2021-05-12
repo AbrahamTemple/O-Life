96748044d9fd573b14a2299702369b7d20c67222
Abraham Vong <73206726+AbrahamTemple@users.noreply.github.com>
2021-05-12 20:50:28 +0800

    Add files via upload

--- project/app/src/main/java/com/example/myapplication/ui/ViewActivity.java
+++ project/app/src/main/java/com/example/myapplication/ui/ViewActivity.java
@@ -1,11 +1,17 @@
 package com.example.myapplication.ui;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
@@ -15,23 +21,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import com.example.myapplication.R;
-import com.example.myapplication.domain.Counter;
-import com.example.myapplication.event.MyAmqp;
+import com.example.myapplication.service.AmqpService;
 import com.google.android.material.button.MaterialButton;
-import com.rabbitmq.client.AMQP;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.DefaultConsumer;
-import com.rabbitmq.client.Envelope;
 import com.yalantis.taurus.PullToRefreshView;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
-import java.io.IOException;
-import java.util.concurrent.TimeoutException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -41,50 +42,42 @@ public class ViewActivity extends AppCompatActivity implements View.OnClickListe
 
     public static final int REFRESH_DELAY = 3000;
 
-    private ConnectionFactory factory;
-    private String userName = "admin" ;
-    private String passWord = "admin" ;
-    private String hostName = "127.0.0.1" ;
-    private String virtualHost = "/";
-    private int portNum = 5672 ;
-    private String exchangeName = "android_exchange" ;
-    private String queueName = "android_queue";
-    private String rountingKey = "news";
-
     @BindView(R.id.pull_to_refresh)
     PullToRefreshView prv;
 
     @BindView(R.id.call_btn)
     MaterialButton call;
 
+    @BindView(R.id.publish_btn)
+    Button pub_btn;
+
+    @BindView(R.id.message_list)
+    TextView msg_list;
+
+    @BindView(R.id.message_text)
+    EditText msg_text;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
         ButterKnife.bind(this);
-        EventBus.getDefault().register(this);
         init();
-        setupConnectionFactory();
-        new Thread(() -> basicConsume()).start();
     }
 
     public void init(){
         prv.setOnRefreshListener(() -> prv.postDelayed(() -> prv.setRefreshing(false), REFRESH_DELAY));
         call.setOnClickListener(this);
+        pub_btn.setOnClickListener(this);
+        AmqpService.startListener(this);
     }
 
-    private void setupConnectionFactory() {
-        factory = new ConnectionFactory();
-        factory.setUsername(userName);
-        factory.setPassword(passWord);
-        factory.setHost(hostName);
-        factory.setPort(portNum);
-        factory.setVirtualHost(virtualHost);
-    }
-
+    @SuppressLint({"SimpleDateFormat", "SetTextI18n"})
     @Subscribe(threadMode = ThreadMode.ASYNC)
     public void OnEventProgress(String msg){
-        System.out.println(msg);
+        Date now = new Date();
+        SimpleDateFormat ft = new SimpleDateFormat ("hh:mm:ss");
+        msg_list.setText(ft.format(now) + ' ' + msg );
     }
 
     @Override
@@ -93,7 +86,11 @@ public class ViewActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.call_btn:
                 getPermission();
                 break;
-             default:
+            case R.id.publish_btn:
+                String msg = msg_text.getText().toString();
+                AmqpService.startPublish(this,msg);
+                break;
+            default:
         }
     }
 
@@ -118,50 +115,32 @@ public class ViewActivity extends AppCompatActivity implements View.OnClickListe
     /* 只有同意打开相关权限才可以开启本程序 */
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case 1:
-                if (grantResults.length > 0) {
-                    for (int result : grantResults) {
-                        if (result != PackageManager.PERMISSION_GRANTED) {
-                            Toast.makeText(this, "必须同意所有权限才能使用本程序", Toast.LENGTH_SHORT).show();
-                            finish();
-                            return;
-                        }
+        if (requestCode == 1) {
+            if (grantResults.length > 0) {
+                for (int result : grantResults) {
+                    if (result != PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "必须同意打开权限才能使用一键拨号", Toast.LENGTH_SHORT).show();
+                        finish();
+                        return;
                     }
-                    call();
-                } else {
-                    Toast.makeText(this, "发生未知错误", Toast.LENGTH_SHORT).show();
-                    finish();
                 }
-                break;
-            default:
+                call();
+            } else {
+                Toast.makeText(this, "发生未知错误", Toast.LENGTH_SHORT).show();
+                finish();
+            }
         }
     }
 
-    private void basicConsume(){
+    @Override
+    protected void onStart() {
+        EventBus.getDefault().register(this);
+        super.onStart();
+    }
 
-        try {
-            Connection connection = factory.newConnection() ;
-            final Channel channel = connection.createChannel() ;
-            channel.exchangeDeclare(exchangeName , "direct" , true) ;
-            channel.queueBind(queueName , exchangeName , rountingKey) ;
-            /**
-             * @param queue 队列名
-             * @param autoAck true:自动应答, false:手动应答
-             * @param callback 构造新实例并记录其与传入通道的关联
-             */
-            channel.basicConsume(queueName , false ,  new DefaultConsumer(channel){
-                @Override
-                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
-                    super.handleDelivery(consumerTag, envelope, properties, body);
-
-                    String msg = new String(body) ;
-                    channel.basicAck(envelope.getDeliveryTag() , false); //应答
-                    EventBus.getDefault().post(msg); //发送到消息总线
-                }
-            });
-        } catch (IOException | TimeoutException e) {
-            e.printStackTrace();
-        }
+    @Override
+    protected void onDestroy() {
+        EventBus.getDefault().unregister(this);
+        super.onDestroy();
     }
 }
